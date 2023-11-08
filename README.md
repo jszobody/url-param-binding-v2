8519af2b8a0c1ffafc96beb22f93bd51ea9031b9
Joseph Szobody <joseph@stechstudio.com>
2023-11-08 09:22:37 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## URL Param Binding
+
+This repo provides a minimal working example of url parameter binding in Livewire v2.
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>URL Binding</title>
+        @livewireStyles
+    </head>
+    <body>
+        {{ $slot }}
+        @livewireScripts
+    </body>
+</html>
